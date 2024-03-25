855a3e9386ad773ae5a77940879ea2dd1df91bff
Maksym <148101061+mbliakharskyi@users.noreply.github.com>
2024-03-25 02:39:35 +0200

    Update env.ts

--- apps/sumologic/src/env.ts
+++ apps/sumologic/src/env.ts
@@ -1,36 +1,5 @@
 import { z } from 'zod';
 
-const zEnvRetry = (defaultValue = '3') =>
-  z
-    .string()
-    .transform(Number)
-    .pipe(z.number().int().min(0).max(20))
-    .default(defaultValue) as unknown as z.ZodUnion<
-    [
-      z.ZodLiteral<0>,
-      z.ZodLiteral<1>,
-      z.ZodLiteral<2>,
-      z.ZodLiteral<3>,
-      z.ZodLiteral<4>,
-      z.ZodLiteral<5>,
-      z.ZodLiteral<6>,
-      z.ZodLiteral<7>,
-      z.ZodLiteral<8>,
-      z.ZodLiteral<9>,
-      z.ZodLiteral<10>,
-      z.ZodLiteral<11>,
-      z.ZodLiteral<12>,
-      z.ZodLiteral<13>,
-      z.ZodLiteral<14>,
-      z.ZodLiteral<15>,
-      z.ZodLiteral<16>,
-      z.ZodLiteral<17>,
-      z.ZodLiteral<18>,
-      z.ZodLiteral<19>,
-      z.ZodLiteral<20>,
-    ]
-  >;
-
 export const env = z
   .object({
     ELBA_API_KEY: z.string().min(1),
