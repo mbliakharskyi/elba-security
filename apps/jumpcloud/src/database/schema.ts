937121c884da064cde9083089879a2d66eb6a2bb
mbliakharskyi <mbliakharskyi@gmail.com>
2024-04-02 18:16:55 +0300

    fix: remove jumpcloud

--- apps/jumpcloud/src/database/schema.ts
+++
@@ -1,8 +0,0 @@
-import { uuid, text, pgTable, timestamp } from 'drizzle-orm/pg-core';
-
-export const Organisation = pgTable('organisation', {
-  id: uuid('id').primaryKey(),
-  region: text('region').notNull(),
-  apiKey: text('apiKey').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-});
--- apps/jumpcloud/src/inngest/functions/index.ts
+++
@@ -1,4 +0,0 @@
-import { synchronizeUsers } from './users/synchronize-users';
-import { scheduleUsersSynchronize } from './users/schedule-users-synchronize'; 
-
-export const inngestFunctions = [synchronizeUsers, scheduleUsersSynchronize];
