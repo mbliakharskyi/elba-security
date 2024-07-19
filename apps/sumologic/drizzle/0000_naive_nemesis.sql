1f431ce91ef9f14da9bc6d004e04dc3c08032e0b
mbliakharskyi <mbliakharskyi@gmail.com>
2024-07-19 18:50:17 +0300

    feat: update the codebase

--- apps/sumologic/drizzle/0000_naive_nemesis.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "organisation" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"region" text NOT NULL,
-	"accessId" text NOT NULL,
-	"accessKey" text NOT NULL,
-	"sourceRegion" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
