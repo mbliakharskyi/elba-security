1b2a80dc50f9ea190d84416d1272de8fee681fe7
mbliakharskyi <mbliakharskyi@gmail.com>
2024-08-07 12:35:35 +0300

    feat: remove unnecessary files

--- apps/statsig/drizzle/0000_sour_betty_ross.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "organisations" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"api_key" text NOT NULL,
-	"region" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
